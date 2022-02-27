42f941b0071bcb3d83b12fccc35b2c94980fe2dc
Your Name <you@example.com>
2022-02-27 00:09:40 +0000

    Css

--- main.go
+++ main.go
@@ -13,21 +13,56 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 		r.URL.Path = "/"
 	}
 
-	tpl := template.Must(template.ParseGlob("templates/homepage.html"))
+	tplHome := template.Must(template.ParseGlob("templates/homepage.html"))
 
-	if err := tpl.Execute(w,nil); err != nil {
+	if err := tplHome.Execute(w,nil); err != nil {
 		log.Fatal(err.Error())
 	}
+}
 
+func GoLang(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/Go-Lang" {
+		r.URL.Path = "/Go-Lang"
+	}
 
+	tplGoLang := template.Must(template.ParseGlob("templates/golang.html"))
+	
+	if err := tplGoLang.Execute(w,nil); err != nil{
+		log.Fatal(err.Error())
+	}
+}
 
+func JavaScript(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/JavaScript" {
+		r.URL.Path = "/JavaScript"
+	}
+	
+	tplJavaScript := template.Must(template.ParseGlob("templates/javascript.html"))
 
+	if err := tplJavaScript.Execute(w,nil); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func Rust(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/Rust" {
+		r.URL.Path = "/Go-Lang"
+	}
+
+	tplRust := template.Must(template.ParseGlob("templates/rust.html"))
+
+	if err := tplRust.Execute(w,nil); err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 func main() {
 
 	http.HandleFunc("/", HomePage)
-	
+	http.HandleFunc("/Go-Lang", GoLang)
+	http.HandleFunc("/JavaScript", JavaScript)
+	http.HandleFunc("/Rust", Rust)
+
 	http.ListenAndServe(":3000", nil)
 }
 
