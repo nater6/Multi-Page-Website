00b61f3bea79b146b2588991466efd27247a53d0
nater68 <n.d.russell@hotmail.com>
2022-08-26 23:05:58 +0100

    Ascii-art-Web page started

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,40 +7,49 @@ import (
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		// http.Error(w, "404 Status not found", http.StatusNotFound)
-		tplErrPage := template.Must(template.ParseGlob("templates/error404.html"))
-
-	if tplErr := tplErrPage.Execute(w, nil); tplErr != nil {
-		fmt.Println("Error Executing the 404 Page",tplErr.Error())
+	if CheckURL("/", w, r) {
+		return
 	}
-		return 
-	}
-
-	tplHome := template.Must(template.ParseGlob("templates/index.html"))
-
-	if err := tplHome.Execute(w, nil); err != nil {
-		fmt.Println("Error Executing the homepage.html template",err.Error())
+	if ExecuteTemp("templates/index.html", w) {
+		return
 	}
 }
 
 func MakeYourGame(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/makeyourgame" {
-		http.Error(w, "404 Status not found", http.StatusNotFound)
-	tplErrPage := template.Must(template.ParseGlob("templates/error404.html"))
-	if tplErr := tplErrPage.Execute(w, nil); tplErr != nil {
-		fmt.Println("Error Executing the 404 Page",tplErr.Error())
-	}
-	return 
+	if CheckURL("/makeyourgame", w, r) {
+		return
 	}
 
-	tplHome := template.Must(template.ParseGlob("templates/makeyourgame.html"))
-
-	if err := tplHome.Execute(w, nil); err != nil {
-		fmt.Println("Error Executing the homepage.html template",err.Error())
+	if ExecuteTemp("templates/makeyourgame.html", w) {
+		return
 	}
 }
 
+func AsciiArtWeb(w http.ResponseWriter, r *http.Request) {
+	if CheckURL("/ascii-art-web", w, r) {
+		return
+	}
 
+	if ExecuteTemp("templates/asciiartweb.html", w){
+		return
+	}
+}
 
+func CheckURL(url string, w http.ResponseWriter, r *http.Request) bool {
+	if r.URL.Path != url {
+		http.Error(w, "404 Status not found", http.StatusNotFound)
+		return true
+	}
+	return false
+}
 
+func ExecuteTemp(path string, w http.ResponseWriter) bool {
+	tpl := template.Must(template.ParseGlob(path))
+
+	if err := tpl.Execute(w, nil); err != nil {
+		fmt.Println("Error Executing the template at: "+path, err.Error())
+		http.Error(w, "500 Status not found", http.StatusInternalServerError)
+		return true
+	}
+	return false
+}
--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs)) // handling the CSS
 	http.HandleFunc("/", handlers.HomePage)
 	http.HandleFunc("/makeyourgame", handlers.MakeYourGame)
+	http.HandleFunc("/ascii-art-web", handlers.AsciiArtWeb)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 	fmt.Println("Starting Port :8080")
 }
